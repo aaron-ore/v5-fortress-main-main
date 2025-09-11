1da73fabee6e570d7b8a2454266d31508dd283b7
[dyad] <ronorejola15@gmail.com>
2025-09-10 22:02:42 -0400

    [dyad] Added a test console log to main.tsx to verify console output after rebuild. - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 
+console.log("Dyad Test Log: main.tsx is running!"); // NEW: Added test log
+
 createRoot(document.getElementById("root")!).render(<App />);
