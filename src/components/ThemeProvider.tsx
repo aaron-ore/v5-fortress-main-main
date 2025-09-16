b006e8a18964b5a3b3351c2c4afdae71adab5cbe
[dyad] <ronorejola15@gmail.com>
2025-09-15 23:37:35 -0400

    [dyad] Removed the deep-forest-light theme - wrote 2 file(s)

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -2,5 +2,5 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props} attribute="class" themes={['dark', 'emerald', 'deep-forest', 'deep-forest-light']}>{children}</NextThemesProvider>;
+  return <NextThemesProvider {...props} attribute="class" themes={['dark', 'emerald', 'deep-forest']}>{children}</NextThemesProvider>;
 }
