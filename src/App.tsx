574a3ed64df02570c7c5f36252093789d48e3251
[dyad] <ronorejola15@gmail.com>
2025-09-19 00:40:09 -0400

    [dyad] wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Toaster as SonnerToaster } from "sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // Added QueryClientProvider import
 import { BrowserRouter } from "react-router-dom";
 import { OnboardingProvider } from "./context/OnboardingContext";
 import { ProfileProvider } from "./context/ProfileContext";
