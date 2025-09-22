70d48755b2294166ba474387cb1af2375cae5435
[dyad] <ronorejola15@gmail.com>
2025-09-22 14:19:06 -0400

    [dyad] wrote 2 file(s)

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -2,5 +2,5 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props} attribute="class" themes={['dark', 'emerald', 'deep-forest', 'tropical-indigo', 'light-grey-indigo']}>{children}</NextThemesProvider>;
+  return <NextThemesProvider {...props} attribute="class" themes={['dark', 'emerald', 'deep-forest', 'tropical-indigo']}>{children}</NextThemesProvider>;
 }
