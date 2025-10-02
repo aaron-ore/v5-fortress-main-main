e066883e4aad18c32f39915351fbad2110860bb5
[dyad] <ronorejola15@gmail.com>
2025-10-01 23:28:25 -0400

    [dyad] wrote 1 file(s)

--- src/pages/Integrations.tsx
+++ src/pages/Integrations.tsx
@@ -503,8 +503,9 @@ const Integrations: React.FC = () => {
 
       <Card className="bg-card border-border rounded-lg shadow-sm p-6">
         <CardHeader className="pb-4 flex flex-row items-center gap-4">
-          <img src="/Intuit_QuickBooks_logo.png" alt="QuickBooks Logo" className="h-10 object-contain" />
-          <CardTitle className="text-xl font-semibold"></CardTitle> {/* Changed here */}
+          <CardTitle className="text-xl font-semibold">
+            <img src="/Intuit_QuickBooks_logo.png" alt="QuickBooks Logo" className="h-10 object-contain" />
+          </CardTitle>
         </CardHeader>
         <CardContent className="space-y-4">
           {isQuickBooksConnected ? (
@@ -555,8 +556,9 @@ const Integrations: React.FC = () => {
 
       <Card className="bg-card border-border rounded-lg shadow-sm p-6">
         <CardHeader className="pb-4 flex flex-row items-center gap-4">
-          <img src={shopifyLogoSrc} alt="Shopify Logo" className="h-10 object-contain" />
-          <CardTitle className="text-xl font-semibold"></CardTitle> {/* Changed here */}
+          <CardTitle className="text-xl font-semibold">
+            <img src={shopifyLogoSrc} alt="Shopify Logo" className="h-10 object-contain" />
+          </CardTitle>
         </CardHeader>
         <CardContent className="space-y-4">
           {isShopifyConnected ? (
