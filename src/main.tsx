f1ad5284577b0478e689b16d72c7d86bd0162bcf
[dyad] <ronorejola15@gmail.com>
2025-10-06 22:33:18 -0400

    [dyad] wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 
-console.log("Dyad Test Log: main.tsx is running!"); // NEW: Added test log
+// Conditionally disable console.log in production
+if (import.meta.env.PROD) {
+  console.log = () => {};
+  console.warn = () => {};
+  console.error = () => {}; // You might want to keep console.error for critical issues
+}
+
+console.log("Dyad Test Log: main.tsx is running!"); // This log will only appear in development
 
 createRoot(document.getElementById("root")!).render(<App />);
